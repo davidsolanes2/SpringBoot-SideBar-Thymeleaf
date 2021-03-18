6b21299d497c05f65541800314b1999567320d4f
David Solanes <davidsolanes2@gmail.com>
2021-03-18 12:02:40 +0100

    initial commit

---
+++ src/main/java/com/app/dracmagicv5/Dracmagicv5Application.java
@@ -0,0 +1,13 @@
+package com.app.dracmagicv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dracmagicv5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dracmagicv5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/dracmagicv5/Dracmagicv5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dracmagicv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dracmagicv5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
